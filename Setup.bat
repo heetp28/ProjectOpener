a5924a72212cbf48baa94a5937983440412c15d4
heetp28 <dhruhee1428@gmail.com>
2025-08-24 20:26:08 +0530

    Added Startup option to setup

--- Setup.bat
+++ Setup.bat
@@ -12,3 +12,17 @@ pyinstaller --onefile --noconsole --icon=file.ico Main.py
 
 copy "file.ico" ".\dist\"
 copy "directory.json" ".\dist\"
+
+:AskStartUp
+set /p choice = "Add as Start App (y/n): "
+if /i "%choice%" == "y" goto AddStartUp
+if /i "%choice%" == "n" goto SkipStartUp
+echo Enter a Valid Option y/n.
+goto AskStartUp
+
+set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+set SHORTCUT_NAME=ProjectOpener.lnk
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP_FOLDER%%SHORTCUT_NAME%');$s.TargetPath='%CD%\dist\Main.exe';$s.Save()"
+
+:SkipStartUp
+echo done (you can't even read this message)
