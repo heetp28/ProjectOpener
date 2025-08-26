20ca56d9d87a907ed04e43cc3cb262f8a52af883
heetp28 <dhruhee1428@gmail.com>
2025-08-26 19:51:32 +0530

    updated setup.bat

--- Setup.bat
+++ Setup.bat
@@ -14,15 +14,15 @@ copy "file.ico" ".\dist\"
 copy "directory.json" ".\dist\"
 
 :AskStartUp
-set /p choice = "Add as Start App (y/n): "
-if /i "%choice%" == "y" goto AddStartUp
-if /i "%choice%" == "n" goto SkipStartUp
+set /p choice="Add as Start App (y/n): "
+if /i "%choice%"=="y" goto AddStartUp
+if /i "%choice%"=="n" goto SkipStartUp
 echo Enter a Valid Option y/n.
 goto AskStartUp
 
 set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 set SHORTCUT_NAME=ProjectOpener.lnk
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP_FOLDER%%SHORTCUT_NAME%');$s.TargetPath='%CD%\dist\Main.exe';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP_FOLDER%/%SHORTCUT_NAME%');$s.TargetPath='%CD%\dist\Main.exe';$s.Save()"
 
 :SkipStartUp
 echo done (you can't even read this message)
